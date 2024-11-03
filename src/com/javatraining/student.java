c1055ea51093ab511f681bbc541ecd55c32736b2
FABIHA <141540675+FABIHASIRAJ@users.noreply.github.com>
2024-11-03 01:09:36 -0800

    changes

---
+++ src/com/javatraining/student.java
@@ -0,0 +1,22 @@
+package com.javatraining;
+import java.util.Scanner;
+
+public class student {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        // Check if the number is even or odd
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+        
+        scanner.close();
+    }
+}
+
+
